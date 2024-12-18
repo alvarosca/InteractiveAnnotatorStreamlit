a3c4a97470a7cb90f720c1caf1e04e8ce9840439
alvarosca <alvaro.sca8@gmail.com>
2024-12-18 09:55:07 -0300

    First commit v4

--- main.py
+++ main.py
@@ -10,12 +10,6 @@ import pandas as pd
 label_list = ['Positivo', 'Negativo', 'No importante']
 actions = ['Agregar', 'Borrar']
 
-if 'patches' not in st.session_state:
-    st.session_state['patches'] = None  # Store generated patches here
-
-if 'patch_index' not in st.session_state:
-    st.session_state['patch_index'] = 0  # Store selected patch index
-
 if 'label' not in st.session_state:
     st.session_state['label'] = 0  # Store selected label
 
@@ -28,11 +22,11 @@ if 'all_points' not in st.session_state:
 if 'all_labels' not in st.session_state:
     st.session_state['all_labels'] = {}  # Dictionary to track labels for each unique point
 
-if 'patch_points' not in st.session_state:
-    st.session_state['patch_points'] = []
+if 'points' not in st.session_state:
+    st.session_state['points'] = []
 
-if 'patch_labels' not in st.session_state:
-    st.session_state['patch_labels'] = []
+if 'labels' not in st.session_state:
+    st.session_state['labels'] = []
 
 # Initialize session state for csv and report data
 if 'csv_data' not in st.session_state:
@@ -42,47 +36,7 @@ if 'report_data' not in st.session_state:
     st.session_state['report_data'] = b""  # Use empty binary to avoid type errors
 
 
-def image_split(image, height=512, width=512, overlap=0):
-    """
-    Splits an image into patches of the same size (height x width) with an optional overlap.
-    """
-    imgs = []
-    image_height = image.shape[0]
-    image_width = image.shape[1]
-    
-    overlap_height = int(height * overlap)
-    overlap_width = int(width * overlap)
-    
-    vertical_splits = int(np.ceil((image_height - overlap_height) / (height - overlap_height)))
-    horizontal_splits = int(np.ceil((image_width - overlap_width) / (width - overlap_width)))
-
-    for y in range(vertical_splits):
-        if ((y + 1) * (height - overlap_height)) < image_height:
-            y_start = y * (height - overlap_height)
-        else:
-            y_start = image_height - height
-
-        for x in range(horizontal_splits):
-            if ((x + 1) * (width - overlap_width)) < image_width:
-                x_start = x * (width - overlap_width)
-            else:
-                x_start = image_width - width
-
-            split = image[y_start:y_start + height, x_start:x_start + width]
-            imgs.append(split)
-
-    return imgs, vertical_splits, horizontal_splits
-
-
-def update_patch_data(session_state, patch_width, patch_height, scale):
-
-    current_patch_index = session_state['patch_index']
-    vert_splits = session_state['vert_splits']
-    hor_splits = session_state['hor_splits']
-    overlap = session_state['overlap']
-
-    current_patch_x_coord = (current_patch_index % hor_splits) * ( patch_width * (1-overlap) )
-    current_patch_y_coord = (current_patch_index // vert_splits) * ( patch_height * (1-overlap) )
+def update_patch_data(session_state, scale):
 
     all_points = session_state['all_points'] # Set to track unique point
     all_labels = session_state['all_labels'] # Dictionary to track labels for each unique point
@@ -90,28 +44,20 @@ def update_patch_data(session_state, patch_width, patch_height, scale):
     all_points = list(all_points)
     all_labels = [all_labels[point] for point in all_points]
 
-    patch_points = []
-    patch_labels = []
+    points = []
+    labels = []
 
     for point, label in zip(all_points, all_labels):
         x, y = point
 
-        x -= current_patch_x_coord
-        y -= current_patch_y_coord
+        x *= scale[0]
+        y *= scale[1]
 
-        if ( x>=0 and y>=0 and x<patch_width and y<patch_height ):
+        points.append(point)
+        labels.append(label)
 
-            x *= scale[0]
-            y *= scale[1]
-
-            point = [x, y]
-
-            patch_points.append(point)
-            patch_labels.append(label)
-
-
-    session_state['patch_points'] = patch_points
-    session_state['patch_labels'] = patch_labels
+    session_state['points'] = points
+    session_state['labels'] = labels
 
 
 def update_results(session_state, file_name):
@@ -136,12 +82,18 @@ def update_results(session_state, file_name):
     num_positive = all_labels.count(0)
     num_negative = all_labels.count(1)
 
+    total = num_positive + num_negative
+
+    if total==0:
+        total = -1
+
     report_content = f"""
     Reporte de anotación
     ==================
     Nombre de la imagen: {file_name}
-    Número de puntos positivos: {num_positive}
-    Número de puntos negativos: {num_negative}
+    Número de puntos positivos: {num_positive} - Porcentaje: {100*num_positive/total}%
+    Número de puntos negativos: {num_negative} - Porcentaje: {100*num_negative/total}%
+    Cantidad total de elementos {total}
     """
 
     # Create file-like object to download the report
@@ -153,18 +105,10 @@ def update_results(session_state, file_name):
     session_state['report_data'] = report_data
 
 
-def update_annotations(new_labels, session_state, patch_width, patch_height, scale):
+def update_annotations(new_labels, session_state, scale):
 
     all_points = session_state['all_points'] # Set to track unique point
     all_labels = session_state['all_labels'] # Dictionary to track labels for each unique point
-
-    current_patch_index = session_state['patch_index']
-    vert_splits = session_state['vert_splits']
-    hor_splits = session_state['hor_splits']
-    overlap = session_state['overlap']
-
-    current_patch_x_coord = (current_patch_index % hor_splits) * ( patch_width * (1-overlap) )
-    current_patch_y_coord = (current_patch_index // vert_splits) * ( patch_height * (1-overlap) )
 
     patch_points = []
 
@@ -178,9 +122,6 @@ def update_annotations(new_labels, session_state, patch_width, patch_height, sca
         label_id = v['label_id']
         patch_points.append([x, y])
 
-        x += current_patch_x_coord
-        y += current_patch_y_coord
-
         point_tuple = (x, y)
 
         if point_tuple not in all_points:
@@ -192,25 +133,19 @@ def update_annotations(new_labels, session_state, patch_width, patch_height, sca
     for global_point in all_points:
         x, y = global_point
 
-        x -= current_patch_x_coord
-        y -= current_patch_y_coord
+        remove_flag = True
 
-        remove_flag = False
+        for patch_point in patch_points:
 
-        if ( x>=0 and y>=0 and x<patch_width and y<patch_height ):
+            x_patch, y_patch = patch_point
 
-            remove_flag = True
+            nequal_flag = not ( (x == x_patch) and (y == y_patch) )
 
-            for patch_point in patch_points:
-
-                x_patch, y_patch = patch_point
-
-                nequal_flag = not ( (x == x_patch) and (y == y_patch) )
-
-                remove_flag = remove_flag and nequal_flag
+            remove_flag = remove_flag and nequal_flag
 
         if remove_flag:
             removed_points.append(global_point)
+
 
     for removed_point in removed_points:
         all_points.remove(removed_point)
@@ -221,8 +156,6 @@ def update_annotations(new_labels, session_state, patch_width, patch_height, sca
 
 
 def main():
-
-    overlap = 0.5
 
     display_size = [1024, 1024]
 
@@ -247,114 +180,66 @@ def main():
         uploaded_file_name = uploaded_file.name[:-4]
 
         # Open the uploaded image using PIL
-        complete_image = Image.open(uploaded_file)
+        image = Image.open(uploaded_file)
+
+        image_size = image.size
+
+        scale  = [ display_size[0]//image_size[0],
+                   display_size[1]//image_size[1]] 
+
+        # Resize the image to 1024x1024
+        image = image.resize(display_size)
+
+        image.save(uploaded_file.name)
         
-        # Display the complete image before any processing
-        st.image(complete_image, caption="Imagen seleccionada", use_column_width=True)
+        update_patch_data(st.session_state, scale)
+
+        img_path = uploaded_file.name
+
+        action = st.session_state['action']
+        if action == actions[1]:
+            mode = 'Del'
+        else:
+            mode = 'Transform'
+                    
+        # Use pointdet to annotate the image
+        new_labels = pointdet(
+            image_path=img_path,
+            label_list=label_list,
+            points=st.session_state['points'],
+            labels=st.session_state['labels'],
+            width = 2048,
+            height = 2048,
+            use_space=True,
+            key=img_path,
+            mode = mode,
+            label = st.session_state['label'],
+            point_width=5
+        )
         
-        # Let the user select patch resolution
-        col1, col2 = st.columns(2)
-        patch_height = col1.number_input("Seleccionar altura de la sub-imagen", \
-                min_value=16, max_value=complete_image.size[1], value=512, step=16)
-        patch_width = col2.number_input("Seleccionar ancho de la sub-imagen", \
-                min_value=16, max_value=complete_image.size[0], value=512, step=16)
+        # Update points and labels in session state if any changes are made
+        if new_labels is not None:
+            update_annotations(new_labels, st.session_state, scale)
+            update_results(st.session_state, uploaded_file_name)
 
-        scale = [ display_size[0]//patch_width,
-                  display_size[1]//patch_height ]
-
-        if st.button("Generar sub-imágenes"):
-            splits, vert_splits, hor_splits = \
-                image_split( np.array(complete_image), 
-                    height=patch_height, width=patch_width, overlap=overlap)
-            st.session_state['vert_splits'] = vert_splits
-            st.session_state['hor_splits'] = hor_splits
-            st.session_state['overlap'] = overlap
-            st.session_state['patches'] = splits
-            st.session_state['patch_index'] = 0  # Reset to first patch
-            for i in range(len(splits)):
-                # Convert to a format that can be used in pointdet
-                patch_path = f"patch_{i}.jpg"
-                patch_img = Image.fromarray(st.session_state['patches'][i])
-
-                # Resize the image to 1024x1024
-                resized_img = patch_img.resize(display_size)
-
-                resized_img.save(patch_path)
-
-        if st.session_state['patches'] is not None:
-
-            st.session_state['patch_index'] = st.selectbox(
-                "Seleccionar sub-imagen a mostrar", 
-                options=list(range(len(st.session_state['patches']))), 
-                format_func=lambda x: f"Sub-imagen {x + 1}", 
-                index=st.session_state['patch_index']
+        st.sidebar.header("Resultados")
+        # Sidebar buttons
+        with st.sidebar:
+            # **1st Download Button** - CSV Annotations
+            st.download_button(
+                label="Descargar anotaciones (CSV)",
+                data=st.session_state['csv_data'],
+                file_name=f"{uploaded_file_name}.csv",
+                mime="text/csv"
             )
 
-            st.sidebar.header("Sub-imagen")
-            # Sidebar buttons
-            with st.sidebar:
-
-                col1, col2 = st.columns([2, 2])
-                with col1:
-                    if st.button("Anterior"):
-                        st.session_state['patch_index'] = \
-                            max(0, st.session_state['patch_index'] - 1)
-
-                with col2:
-                    if st.button("Siguiente"):
-                        st.session_state['patch_index'] = \
-                            min(len(st.session_state['patches']) - 1, \
-                                st.session_state['patch_index'] + 1)
-
-            update_patch_data(st.session_state, patch_width, patch_height, scale)
-
-            img_path = f"patch_{st.session_state['patch_index']}.jpg"
-
-            action = st.session_state['action']
-            if action == actions[1]:
-                mode = 'Del'
-            else:
-                mode = 'Transform'
-                        
-            # Use pointdet to annotate the image
-            new_labels = pointdet(
-                image_path=img_path,
-                label_list=label_list,
-                points=st.session_state['patch_points'],
-                labels=st.session_state['patch_labels'],
-                width = display_size[0],
-                height = display_size[1],
-                use_space=True,
-                key=img_path,
-                mode = mode,
-                label = st.session_state['label'],
-                point_width=5
+            # **2nd Download Button** - Annotation Report
+            st.download_button(
+                label="Descargar Reporte (txt)",
+                data=st.session_state['report_data'],
+                file_name=f'{uploaded_file_name}.txt',
+                mime='text/plain'
             )
-            
-            # Update points and labels in session state if any changes are made
-            if new_labels is not None:
-                update_annotations(new_labels, st.session_state, patch_width, patch_height, scale)
-                update_results(st.session_state, uploaded_file_name)
-
-
-            st.sidebar.header("Resultados")
-            # Sidebar buttons
-            with st.sidebar:
-                # **1st Download Button** - CSV Annotations
-                st.download_button(
-                    label="Descargar anotaciones (CSV)",
-                    data=st.session_state['csv_data'],
-                    file_name=f"{uploaded_file_name}.csv",
-                    mime="text/csv"
-                )
-
-                # **2nd Download Button** - Annotation Report
-                st.download_button(
-                    label="Descargar Reporte (txt)",
-                    data=st.session_state['report_data'],
-                    file_name=f'{uploaded_file_name}.txt',
-                    mime='text/plain'
-                )
 
 
 
--- streamlit_image_annotation/__init__.py
+++
@@ -1,3 +0,0 @@
-IS_RELEASE = True
-
-from .Point import pointdet
