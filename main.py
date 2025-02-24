f5aa5ea4ad8ce99df58ccc84cdb9b0a88a9f79be
alvarosca <alvaro.sca8@gmail.com>
2025-02-23 22:05:10 -0300

    Updated annotator

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import streamlit as st
 from image_annotation import *
+from annotation_correction import *
 
 # Folders
 image_dir  = "./images"
 ann_dir    = "./annotations"
 report_dir = "./reports"
 
-app_list = ["Anotación de imágenes", "Corrección de anotaciones", "Corrección de máscaras"]
+app_list = ["Anotación de imágenes", "Corrección de anotaciones"]
 
 # We want the wide mode to be set by default
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 def main():
 
-    # NOT FULLY IMPLEMENTED YET
-    # st.sidebar.header("Seleccionar aplicación")
-    # with st.sidebar:
-    #     st.session_state['Application'] = st.selectbox("Aplicación:", app_list)
+    st.sidebar.header("Seleccionar aplicación")
+    with st.sidebar:
+        st.session_state['Application'] = st.selectbox("Aplicación:", app_list)
 
-    # if st.session_state['Application'] == app_list[0]:
-    #     image_ann(st.session_state)
+    if st.session_state['Application'] == app_list[0]:
+        image_ann(st.session_state)
 
-    image_ann(st.session_state)
+    if st.session_state['Application'] == app_list[1]:
+        ann_correction(st.session_state)
+
 
 if __name__ == "__main__":
     main()
